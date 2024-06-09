81ce656b871e3a91af890674996656dfe5fa5e33
chrishanzlik <senz2702@gmail.com>
2024-06-09 14:53:33 +0200

    Projektdateien hinzufügen.

---
+++ MicMuter.WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MicMuter.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ViewModel = new MainWindowViewModel();
+
+            this.WhenActivated(disposable =>
+            {
+                this.OneWayBind(ViewModel, vm => vm.StatusIcon, v => v.TaskbarIcon.Icon).DisposeWith(disposable);
+            });
+        }
+    }
+}
---
+++ MicMuter.WPF/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicMuter.WPF
+{
+    public class MainWindowViewModel : ReactiveObject, IActivatableViewModel
+    {
+        private Icon? _statusIcon;
+        public Icon? StatusIcon
+        {
+            get => _statusIcon;
+            set => this.RaiseAndSetIfChanged(ref _statusIcon, value);
+        }
+
+        public ViewModelActivator Activator { get; } = new();
+
+        public MainWindowViewModel()
+        {
+            StatusIcon = Resources.RedMicrophone;
+
+            this.WhenActivated(disposable =>
+            {
+                Disposable.Create(() => { /* cleanup */ }).DisposeWith(disposable);
+            });
+        }
+
+    }
+}
